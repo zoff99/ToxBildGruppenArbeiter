eab7a1233c8edecf42e150a8535241b80ca74986
Zoff <zoff99@users.noreply.github.com>
2017-10-02 16:50:12 +0200

    Update update_from_ci.sh

--- src/update_from_ci.sh
+++ src/update_from_ci.sh
@@ -8,5 +8,5 @@ cd $(dirname "$0")
 
 pkill bild_gruppen_arbeiter_static # will stop bild_gruppen_arbeiter_static
 cp -av bild_gruppen_arbeiter_static bild_gruppen_arbeiter_static__BACKUP
-wget -O bild_gruppen_arbeiter_static 'https://circleci.com/api/v1/project/zoff99/ToxCam/latest/artifacts/0/$CIRCLE_ARTIFACTS/ubuntu_14_04_binaries/bild_gruppen_arbeiter_static?filter=successful&branch=master'
+wget -O bild_gruppen_arbeiter_static 'https://circleci.com/api/v1/project/zoff99/ToxBildGruppenArbeiter/latest/artifacts/0/$CIRCLE_ARTIFACTS/ubuntu_14_04_binaries/bild_gruppen_arbeiter_static?filter=successful&branch=master'
 chmod u+rwx bild_gruppen_arbeiter_static
